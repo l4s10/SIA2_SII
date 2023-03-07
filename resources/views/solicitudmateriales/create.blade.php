e7e137896efbb34ff5bbf261376f2525e20808a1
Koshy <franciscomunoz142@gmail.com>
2023-03-07 14:30:25 -0300

    avance en modulo solicitud

---
+++ resources/views/solicitudmateriales/create.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+<!-- TITULO DE LA PESTAÑA -->
+@section('title', 'Solicitar Material')
+
+<!-- CABECERA DE LA PAGINA -->
+@section('content_header')
+    <h1>Solicitud Material</h1>
+@stop
+
+@section('content')
+    <!-- COLOCA AQUI EL FORMULARIO QUE CREES -->
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+@stop
