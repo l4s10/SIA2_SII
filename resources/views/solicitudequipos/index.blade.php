7d6654cf7f753497fb3e468f997e0e39e4aad7c7
Jorge <jorge.valdivia1901@alumnos.ubiobio.cl>
2023-08-04 11:33:10 -0400

    Cambios esteticos y nuevo rol REQUIRENTE.

--- resources/views/solicitudequipos/index.blade.php
+++ resources/views/solicitudequipos/index.blade.php
@@ -9,6 +9,11 @@
     <div><strong>Bienvenido Administrador:</strong> Acceso total al módulo, se dispone las opciones para administrar, modificar, eliminar, las solicitudes de reservas de equipos.<div>
     </div>
     @endrole
+    @role('REQUIRENTE')
+    <div class="alert alert-info" role="alert">
+    <div><strong>Bienvenido Requirente:</strong> En este módulo usted podrá verificar el estado de sus solicitudes (tipo).<div>
+    </div>
+    @endrole
     @role('SERVICIOS')
     <div class="alert alert-info" role="alert">
     <div><strong>Bienvenido Servicio:</strong> En este módulo usted podrá verificar el estado de sus solicitudes (tipo).<div>
