f7b61efe8864a9dfbc42dd80a5cd3d8101c8a5fa
Fr4mTarro <franciscomunoz142@gmail.com>
2023-08-10 21:30:02 -0400

    actualizando seeders

--- database/seeders/MaterialSeeder.php
+++ database/seeders/MaterialSeeder.php
@@ -19,72 +19,84 @@ class MaterialSeeder extends Seeder
             [
                 'ID_MATERIAL' => 1,
                 'ID_TIPO_MAT' => 1,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'ESCOBA',
                 'STOCK' => 20
             ],
             [
                 'ID_MATERIAL' => 2,
                 'ID_TIPO_MAT' => 1,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'CLORO',
                 'STOCK' => 50
             ],
             [
                 'ID_MATERIAL' => 3,
                 'ID_TIPO_MAT' => 2,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'LAPIZ PASTA ROJO',
                 'STOCK' => 93
             ],
             [
                 'ID_MATERIAL' => 4,
                 'ID_TIPO_MAT' => 2,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'LAPIZ PASTA AZUL',
                 'STOCK' => 95
             ],
             [
                 'ID_MATERIAL' => 5,
                 'ID_TIPO_MAT' => 1,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'BOLSAS DE BASURA',
                 'STOCK' => 85
             ],
             [
                 'ID_MATERIAL' => 6,
                 'ID_TIPO_MAT' => 2,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'LAPIZ CORRECTOR',
                 'STOCK' => 63
             ],
             [
                 'ID_MATERIAL' => 7,
                 'ID_TIPO_MAT' => 3,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'MOUSE INALAMBRICO',
                 'STOCK' => 6
             ],
             [
                 'ID_MATERIAL' => 8,
                 'ID_TIPO_MAT' => 3,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'TECLADO INALAMBRICO',
                 'STOCK' => 10
             ],
             [
                 'ID_MATERIAL' => 9,
                 'ID_TIPO_MAT' => 3,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'AURICULARES',
                 'STOCK' => 12
             ],
             [
                 'ID_MATERIAL' => 10,
                 'ID_TIPO_MAT' => 4,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'TELEVISION',
                 'STOCK' => 2
             ],
             [
                 'ID_MATERIAL' => 11,
                 'ID_TIPO_MAT' => 4,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'MICRONDAS',
                 'STOCK' => 2
             ],
             [
                 'ID_MATERIAL' => 12,
                 'ID_TIPO_MAT' => 4,
+                'ID_DIRECCION' => 16,
                 'NOMBRE_MATERIAL' => 'HORNO ELECTRICO',
                 'STOCK' => 2
             ]
--- database/seeders/MaterialTipoSeeder.php
+++ database/seeders/MaterialTipoSeeder.php
@@ -13,10 +13,20 @@ class MaterialTipoSeeder extends Seeder
      */
     public function run(){
         DB::table('tipo_material')->insert([
-            ['ID_TIPO_MAT' => 1, 'TIPO_MATERIAL' => 'ASEO'],
-            ['ID_TIPO_MAT' => 2, 'TIPO_MATERIAL' => 'ESCRITORIO'],
-            ['ID_TIPO_MAT' => 3, 'TIPO_MATERIAL' => 'COMPUTACION'],
-            ['ID_TIPO_MAT' => 4, 'TIPO_MATERIAL' => 'ELECTRODOMESTICOS']
+            ['ID_TIPO_MAT' => 1, 'TIPO_MATERIAL' => 'ASEO', 'ID_DIRECCION' => 16],
+            ['ID_TIPO_MAT' => 2, 'TIPO_MATERIAL' => 'ESCRITORIO', 'ID_DIRECCION' => 16],
+            ['ID_TIPO_MAT' => 3, 'TIPO_MATERIAL' => 'COMPUTACION', 'ID_DIRECCION' => 16],
+            ['ID_TIPO_MAT' => 4, 'TIPO_MATERIAL' => 'ELECTRODOMESTICOS', 'ID_DIRECCION' => 16],
+
+            ['ID_TIPO_MAT' => 5, 'TIPO_MATERIAL' => 'ASEO', 'ID_DIRECCION' => 18],
+            ['ID_TIPO_MAT' => 6, 'TIPO_MATERIAL' => 'ESCRITORIO', 'ID_DIRECCION' => 18],
+            ['ID_TIPO_MAT' => 7, 'TIPO_MATERIAL' => 'COMPUTACION', 'ID_DIRECCION' => 18],
+            ['ID_TIPO_MAT' => 8, 'TIPO_MATERIAL' => 'ELECTRODOMESTICOS', 'ID_DIRECCION' => 18],
+
+            ['ID_TIPO_MAT' => 9, 'TIPO_MATERIAL' => 'ASEO', 'ID_DIRECCION' => 10],
+            ['ID_TIPO_MAT' => 10, 'TIPO_MATERIAL' => 'ESCRITORIO', 'ID_DIRECCION' => 10],
+            ['ID_TIPO_MAT' => 11, 'TIPO_MATERIAL' => 'COMPUTACION', 'ID_DIRECCION' => 10],
+            ['ID_TIPO_MAT' => 12, 'TIPO_MATERIAL' => 'ELECTRODOMESTICOS', 'ID_DIRECCION' => 10]
         ]);
     }
 }
