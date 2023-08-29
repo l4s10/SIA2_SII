e1f8a5023e73f48c4413eb81b4f0f73f98c839cb
Fr4ncois_Kotch <franciscomunoz142@gmail.com>
2023-08-29 10:43:39 -0400

    🆕 diasTramitacion, diasTotales

--- app/Models/SolicitudMateriales.php
+++ app/Models/SolicitudMateriales.php
@@ -24,18 +24,13 @@ class SolicitudMateriales extends Model
         'ESTADO_SOL',
         'MODIFICADO_POR'
     ];
-
-    public function diasDeTramitacion()
-    {
-        if ($this->FECHA_DESPACHO) {
-            $fechaInicio = new Carbon($this->created_at); // Usando el timestamp de creación
-            $fechaFin = new Carbon($this->FECHA_DESPACHO);
-            return $fechaInicio->diffInDays($fechaFin);
-        } else {
-            // La solicitud aún está en proceso, manejar como prefieras
-            return null;
-        }
-    }
+    /*
+        Para los graficos de promedio de dias:
+        -> Calculo de dias en atender solicitud.
+        -> Calculo de dias en despachar.
+        -> Calculo de dias en confirmar recepción.
+    */
+    //Comparamos la fecha de creacion con la actual (para mostrar mientras no se atienda la solicitud)
     public function diasSinAtender()
     {
         $fechaInicio = new Carbon($this->created_at); // Usando el timestamp de creación
@@ -50,5 +45,28 @@ class SolicitudMateriales extends Model
         }
 
         return $dias;  // Diferencia en días hábiles
+    }
+    //Calcula los dias entre la fecha de creacion y la fecha de despacho
+    public function diasDeTramitacion()
+    {
+        if ($this->FECHA_DESPACHO) {
+            $fechaInicio = new Carbon($this->created_at); // Usando el timestamp de creación
+            $fechaFin = new Carbon($this->FECHA_DESPACHO);
+            return $fechaInicio->diffInDays($fechaFin);
+        } else {
+            // La solicitud aún está en proceso, manejar como prefieras
+            return null;
+        }
+    }
+    //Comparamos la fecha inicial con la fecha de recepcion
+    public function diasTotales(){
+        if($this->FECHA_RECEPCION){
+            $fechaInicio = new Carbon($this->created_at);
+            $fechaFin = new Carbon($this->FECHA_RECEPCION);
+            return $fechaInicio->diffInDays($fechaFin);
+        }else{
+            //Manejar que la solicitud aun esta en proceso
+            return null;
+        }
     }
 }
