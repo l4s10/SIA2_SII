19de32141616a505e7c3050ef216da94c5936a36
FranciscoTarroW11 <franciscomunoz142@gmail.com>
2023-12-29 15:20:57 -0300

    Validacion de calendarios

--- public/js/Reportes/Calendarios/range-calendar.js
+++ public/js/Reportes/Calendarios/range-calendar.js
@@ -1,11 +1,16 @@
 $(function () {
-    $('#start-date').flatpickr({
+    var startDatePicker = $('#start-date').flatpickr({
         locale: 'es',
         dateFormat: "Y-m-d",
         altFormat: 'd-m-Y',
         altInput: true,
+        minDate: "2019-01", // establece la fecha mínima a enero de 2019 suponiendo los datos recuperados del SIA V1
+        onChange: function(selectedDates, dateStr, instance) {
+            endDatePicker.set('minDate', selectedDates[0] || null);
+        }
     });
-    $('#end-date').flatpickr({
+
+    var endDatePicker = $('#end-date').flatpickr({
         locale: 'es',
         dateFormat: "Y-m-d",
         altFormat: 'd-m-Y',
